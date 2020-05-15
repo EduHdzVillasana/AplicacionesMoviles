30aeadba6ca83e959ae6f91d4b026feabfd67e78
destinyaguilar19 <64319907+destinyaguilar19@users.noreply.github.com>
2020-05-15 00:49:14 -0500

    Update concurso-in.component.ts

--- PRUEBA1/src/app/concurso-in/concurso-in.component.ts
+++ PRUEBA1/src/app/concurso-in/concurso-in.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { InAppBrowser } from '@ionic-native/in-app-browser/ngx';
 
 @Component({
   selector: 'app-concurso-in',
@@ -7,13 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ConcursoInComponent implements OnInit {
 
-  constructor() {
+  constructor( private iab: InAppBrowser) {
    
    }
 
   ngOnInit() {}
   
-  
+  GoToIngreso(){
+    this.iab.create('https://www.uanl.mx/tramites/concurso-de-ingreso-a-licenciatura/','_system');
+  }
+  //https://www.uanl.mx/wp-content/uploads/2020/01/Guia-EXANI-II-25a-edicion-2020.pdf
+  GoToExani(){
+    this.iab.create('https://www.uanl.mx/wp-content/uploads/2020/01/Guia-EXANI-II-25a-edicion-2020.pdf','_system');
+  }
 
 
 }
