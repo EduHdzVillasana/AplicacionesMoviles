424d18a650414381e008180924793f6a33357819
destinyaguilar19 <64319907+destinyaguilar19@users.noreply.github.com>
2020-05-14 20:24:27 -0500

    Revert "Merge branch 'master' of https://github.com/EduHdzVillasana/AplicacionesMoviles"
    This reverts commit 0123ff4e5c895b5108b166067fea422f8dca1aea, reversing
    changes made to 62ca5ebe38aecde66eeaf1e0839509d881680f9d.

--- PRUEBA1/src/app/concurso-in/concurso-in.component.ts
+++ PRUEBA1/src/app/concurso-in/concurso-in.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { InAppBrowser } from '@ionic-native/in-app-browser/ngx';
 
 @Component({
   selector: 'app-concurso-in',
@@ -8,18 +7,13 @@ import { InAppBrowser } from '@ionic-native/in-app-browser/ngx';
 })
 export class ConcursoInComponent implements OnInit {
 
-  constructor(private iab: InAppBrowser) {
+  constructor() {
    
    }
 
   ngOnInit() {}
   
-  GoToIngreso(){
-    this.iab.create('https://www.uanl.mx/tramites/concurso-de-ingreso-a-licenciatura/','_system');
-  }
-  //https://www.uanl.mx/wp-content/uploads/2020/01/Guia-EXANI-II-25a-edicion-2020.pdf
-  GoToExani(){
-    this.iab.create('https://www.uanl.mx/wp-content/uploads/2020/01/Guia-EXANI-II-25a-edicion-2020.pdf','_system');
-  }
+  
+
 
 }
