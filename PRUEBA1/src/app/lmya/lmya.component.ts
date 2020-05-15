baf106cdac2cac069149922c1d68b6dbf12aa77a
JanyanP <janyan.rodriguez.271@gmail.com>
2020-05-15 17:10:30 -0500

    Botones de reproducción
    Reproducción del audio de las carreras con botones

--- PRUEBA1/src/app/lmya/lmya.component.ts
+++ PRUEBA1/src/app/lmya/lmya.component.ts
@@ -1,12 +1,77 @@
-import { Component, OnInit } from '@angular/core';
-import { ModalController } from '@ionic/angular';
+
 import{ModalLmyaComponent} from '../modal-lmya/modal-lmya.component'
+
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { ModalController, IonRange } from '@ionic/angular';
+import {Howl} from 'howler';
+
+/////////////////////////////////////////
+export interface Track{//////////////////
+  name: string;//////////////////////////
+  path: string;//////////////////////////
+}
+
 @Component({
   selector: 'app-lmya',
   templateUrl: './lmya.component.html',
   styleUrls: ['./lmya.component.scss'],
 })
 export class LmyaComponent implements OnInit {
+  
+playlist: Track[]=[
+  {
+    name: 'Licenciatura en Multimedia y Animación Digital',
+    path: 'assets/fisica.wav'/////////////////////////////////////AGREGAR EXTENSU¿IÓN DEL AUDIO AQUI
+  }
+];
+
+activeTrack: Track=null;
+player: Howl=null;
+isPlaying=false;
+progress=0;
+@ViewChild('range',{static:false})range:IonRange;
+start(track:Track){
+  if(this.player){
+    this.player.stop();
+  }
+  this.player=new Howl({
+    src:[track.path],
+    html5:true,
+    onplay: ()=>{
+      console.log('onPlay');
+      this.isPlaying=true;
+      this.activeTrack=track;
+      this.updateProgress();
+    },
+    onend: ()=>{
+      console.log('onEnd');
+    }
+  });
+  this.player.play();
+}
+tooglePlayer(pause){
+  this.isPlaying=!pause;
+  if(pause){
+    this.player.pause();
+  }else{
+    this.player.play();
+  }
+
+}
+seek(){
+  let newValue=+this.range.value;
+  let duration=this.player.duration();
+  this.player.seek(duration*(newValue/100));
+}
+updateProgress(){
+  let seek=this.player.seek();
+  this.progress=(seek/this.player.duration())*100||0;
+  setTimeout(()=>{
+    this.updateProgress();
+  },1000)
+}
+
+/////////////////////////////////////////////////////////
   dataReturned:any;
   constructor(
     public modalController: ModalController
